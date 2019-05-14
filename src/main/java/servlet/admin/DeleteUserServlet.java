4d8605bd9f17ed620498724637c1d36df6643a2f
Sergey Klunniy <s.klunniy@gmail.com>
2019-05-14 05:27:20 +0300

    home18 14.05.2019 херня а не комит

---
+++ src/main/java/servlet/admin/DeleteUserServlet.java
@@ -0,0 +1,31 @@
+package servlet.admin;
+
+import dao.UserDao;
+import model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(value = "/delete")
+public class DeleteUserServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String password = req.getParameter("password");
+
+        UserDao userDao = new UserDao();
+        User user = new User(name, password);
+        userDao.delUser(user);
+
+        List<User> list = userDao.getAllUsers();
+
+        req.setAttribute("users", list);
+        req.getRequestDispatcher("admin/usersEditDelete.jsp").forward(req, resp);
+    }
+}
